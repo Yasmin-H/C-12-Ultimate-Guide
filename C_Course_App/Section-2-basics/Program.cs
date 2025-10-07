82fea35b784f0645c94833ea39b7c5be8ec740b5
yasmin.haidar <yasmin.haidar@justeattakeaway.com>
2025-10-07 18:55:55 +0100

    Refactor: Introduced correct C# project structure

---
+++ C_Course_App/Section-2-basics/Program.cs
@@ -0,0 +1,11 @@
+
+
+class MyClass  // every csharp program should contain atleast one class 
+{
+    static void Main() // starting execution point of program ( CLR will search this)
+    // void = no return value 
+    // static = Main method not required for its class to be created 
+    {
+        
+    }
+}
